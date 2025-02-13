2101c04050f144226c0bed90001505022e93e615
golap-raj <golapraj47@gmail.com>
2025-02-14 01:09:27 +0600

    add date

---
+++ date.js
@@ -0,0 +1,5 @@
+// const day = new Date ()
+// console.log(day)
+
+const date = new Date ("2026-10-10")
+console.log(date.getMonth())
---
+++ math.js
@@ -0,0 +1,7 @@
+const min = Math.min(1, 33, 55, 667)
+console.log(min)
+
+const big = Math.max(11, 44, 55, 443)
+console.log(big)
+console.log(Math.round(2.25))
+console.log(Math.round(2.75))
